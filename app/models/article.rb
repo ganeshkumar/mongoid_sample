d272faec0aa959cd788ae6c85f89546fb9fd9fce
Ganesh Kumar <ganesh@Ganeshs-iMac-5.local>
2011-06-10 12:46:19 +0530

    initial commit

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article
+  include Mongoid::Document
+  field :name, :type => String
+  field :content, :type => String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidSample::Application.config.secret_token = '3534fd0ae4b6d722daa0daee927283a99615225d07c9662c5a7219d36adf5be4cf0ec48bce0ecea702f9dccd534c32d10ae7e3bcaf7d1729a203e8fe6a0ad878'
