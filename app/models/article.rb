2dab8f629d3ecedd0bd8072eac857d221d96c2c9
ganesh <ganesh.dc@gmail.com>
2011-07-08 14:49:12 +0530

    added the boolean field

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article
   include Mongoid::Document
   field :name, :type => String
   field :content, :type => String
+  field :reviewed, :type => Boolean
 
   validates_uniqueness_of :name, :message => "already exists"
 end
