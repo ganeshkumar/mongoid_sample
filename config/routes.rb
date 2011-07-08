98dd5fd398414d363507cb74879e97b9179eb93b
ganesh <ganesh.dc@gmail.com>
2011-07-08 13:00:10 +0530

    added default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MongoidSample::Application.routes.draw do
   resources :articles
 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +51,7 @@ MongoidSample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "articles/index#index"
 
   # See how all your routes lay out with "rake routes"
 
